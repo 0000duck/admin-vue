03fe17a28a628a2ec1b1152cff3057471af8f482
bluecolordemon <bluecolordemon@qq.com>
2017-05-16 17:44:42 +0800

    2017年5月16日

---
+++ Test_iview.UI/Areas/TestB/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_iview.UI.Areas.TestB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: TestB/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Test_iview.UI/Areas/TestB/TestBAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Test_iview.UI.Areas.TestB
+{
+    public class TestBAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TestB";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TestB_default",
+                "TestB/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
