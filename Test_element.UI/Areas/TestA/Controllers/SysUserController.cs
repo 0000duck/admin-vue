f579d4b87df3833dd1e35bb32822bf55c61ec9f6
bluecolordemon <bluecolordemon@qq.com>
2017-06-26 16:46:18 +0800

    查询

--- Test_element.UI/Areas/TestA/Controllers/SysUserController.cs
+++ Test_element.UI/Areas/TestA/Controllers/SysUserController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Test_element.UI.Areas.TestA.Models;
+using Test_element.UI.Common;
+using Test_element.UI.Entities;
+using Test_element.UI.Models;
 
 namespace Test_element.UI.Areas.TestA.Controllers
 {
@@ -11,6 +16,9 @@ namespace Test_element.UI.Areas.TestA.Controllers
     /// </summary>
     public class SysUserController : Controller
     {
+
+        AdminVueModel db = new AdminVueModel();
+
         // GET: TestA/SysUser
         public ActionResult Index()
         {
@@ -18,9 +26,62 @@ namespace Test_element.UI.Areas.TestA.Controllers
         }
 
 
-        public JsonResult List() { return null; }
+        public JsonResult List(UserListModel model)
+        {
+            int page = model.page;
+            int rowSize = model.rowSize;
+
+            bool sortWay_order = model.sortWay_order;
+            string sortWay_prop = model.sortWay_prop;
+
+            AjaxResult ajaxResult = null;
+
+            var total = db.Users.Count();
+            object users = null;
+
+            if (sortWay_order)
+            {
+                users = db.Users.OrderBy(p => p.NickName).Skip(page * rowSize).Take(rowSize).Select(p => new
+                {
+                    Id = p.Id,
+                    Account = p.Account,
+                    NickName = p.NickName,
+                    Password = p.Password,
+                    Avatar = p.Avatar,
+                    CreateTime = p.CreateTime
+                }).ToList();
+            }
+            else
+            {
+                users = db.Users.OrderByDescending(p => p.NickName).Skip(page * rowSize).Take(rowSize).Select(p => new
+                {
+                    Id = p.Id,
+                    Account = p.Account,
+                    NickName = p.NickName,
+                    Password = p.Password,
+                    Avatar = p.Avatar,
+                    CreateTime = p.CreateTime
+                }).ToList();
+            }
+
+            ajaxResult = new AjaxResult()
+            {
+                Success = true,
+                Msg = "加载数据成功",
+                Body = new
+                {
+                    Total = total,
+                    Users = users
+                }
+            };
+
+            return new JsonResult() { ContentEncoding = Encoding.UTF8, Data = CommonJson.camelJson(ajaxResult), JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
         public JsonResult Add() { return null; }
+
         public JsonResult Edit() { return null; }
+
         public JsonResult Remove() { return null; }
 
         /// <summary>
---
+++ Test_element.UI/Areas/TestA/Models/UserListModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_element.UI.Areas.TestA.Models
+{
+    /// <summary>
+    /// 用户查询参数
+    /// </summary>
+    public class UserListModel
+    {
+        /// <summary>
+        /// 页数
+        /// </summary>
+
+        public int page { get; set; }
+
+        /// <summary>
+        /// 每页显示条目个数
+        /// </summary>
+
+        public int rowSize { get; set; }
+
+        /// <summary>
+        /// 账户
+        /// </summary>
+        public string account { get; set; }
+
+        /// <summary>
+        /// 昵称
+        /// </summary>
+        public string nickName { get; set; }
+
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public string avatar { get; set; }
+
+        /// <summary>
+        /// 开始时间
+        /// </summary>
+        public DateTime? startTime { get; set; }
+
+        /// <summary>
+        /// 结束时间
+        /// </summary>
+        public DateTime? endTime { get; set; }
+
+        /// <summary>
+        /// 排序列名
+        /// </summary>
+        public string sortWay_prop { get; set; }
+
+        /// <summary>
+        /// 排序方式
+        /// </summary>
+        public bool sortWay_order { get; set; }
+    }
+}
